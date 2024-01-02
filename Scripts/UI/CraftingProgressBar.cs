0af5ad9830f625bf0cb4412ef488f24b42b4fa34
林雨晴 <megumin_lyq@outlook.com>
2024-01-02 10:20:35 +0800

    抽取预制体, 修改名字

--- Scripts/UI/CraftingProgressBar.cs
+++ Scripts/UI/CraftingProgressBar.cs
@@ -4,9 +4,10 @@ namespace ZhaoHuiSoftware.VoxelPlayMod.CraftingTable.UI
 {
     public class CraftingProgressBar : MonoBehaviour
     {
+        [Tooltip("The fill area of progress bar")]
         [SerializeField] protected RectTransform m_Fill;
 
-        [Tooltip("Dont edit, change at runtime")]
+        [Tooltip("Dont edit, change at runtime\n, The x coord when it's empty")]
         [SerializeField] protected float m_EmptyPositionOfX;
 
         private void Awake()
@@ -14,6 +15,10 @@ namespace ZhaoHuiSoftware.VoxelPlayMod.CraftingTable.UI
             m_EmptyPositionOfX = m_Fill.localPosition.x - m_Fill.sizeDelta.x;
         }
 
+        /// <summary>
+        /// Update the progress of the progressbar
+        /// </summary>
+        /// <param name="progress">the progress 0~1</param>
         public void UpdateProgress(float progress)
         {
             var x = (m_Fill.sizeDelta.x * progress) + m_EmptyPositionOfX;
